4ecb4f24a03d42ec245f23d3f1f44c84f177a1f8
APachon10 <Pachondestroy95@gmail.com>
2019-03-18 17:02:37 +0100

    Comenzando implementacion de DAO y creando Ventana Baraja Cartas

--- src/daoVentanas/PantallaBaraja.java
+++
@@ -1,5 +0,0 @@
-package daoVentanas;
-
-public class PantallaBaraja {
-	
-}	
---
+++ src/daoVentanas/Pantalla_BarajaCartas.java
@@ -0,0 +1,14 @@
+package daoVentanas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Pantalla_BarajaCartas extends JFrame {
+
+	private JPanel contentPane;
+
+}
