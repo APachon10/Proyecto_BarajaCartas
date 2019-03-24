cf7c610cd06634eb68a3b201ae30d47d831d5aa7
Alberto Pachon <Pachondestroy95@gmail.com>
2019-03-25 00:31:04 +0100

    Se cargan las cartas en la lista , pero hay problemas a la hora de añadirlas

--- src/daoVentanas/Pantalla_BarajaCartas.java
+++ src/daoVentanas/Pantalla_BarajaCartas.java
@@ -4,14 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -21,6 +25,8 @@ public class Pantalla_BarajaCartas extends JFrame {
 	private JPanel contentPane;
 	private JTextField Nombre_Baraja;	
 	private static JList SeleccionCartas,Cartas_Mazo;
+	private JButton Insertar;
+	private int contador;
 	public Pantalla_BarajaCartas() {
 		setTitle("Crear Baraja");
 
@@ -28,7 +34,7 @@ public class Pantalla_BarajaCartas extends JFrame {
 
 		Cartas_Mazo = new JList();
 
-		JButton Insertar = new JButton("---->");
+		Insertar = new JButton("---->");
 
 		JButton Quitar = new JButton("<-----");
 
@@ -40,14 +46,24 @@ public class Pantalla_BarajaCartas extends JFrame {
 		JButton BuscarBaraja = new JButton("Buscar");
 
 		JButton BarajaAleatoria = new JButton("Random");
+
+		JButton btnLoadButton = new JButton("Load");
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 				groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 						.addGap(47)
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(SeleccionCartas, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblNombreBaraja)
+										.addGap(26)
+										.addComponent(Nombre_Baraja, GroupLayout.PREFERRED_SIZE, 303, GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.UNRELATED)
+										.addComponent(BuscarBaraja))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+												.addComponent(btnLoadButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addComponent(SeleccionCartas, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE))
 										.addPreferredGap(ComponentPlacement.RELATED)
 										.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 												.addGroup(groupLayout.createSequentialGroup()
@@ -56,15 +72,11 @@ public class Pantalla_BarajaCartas extends JFrame {
 												.addGroup(groupLayout.createSequentialGroup()
 														.addComponent(Quitar, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE)
 														.addPreferredGap(ComponentPlacement.RELATED)))
-										.addComponent(Cartas_Mazo, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE))
-								.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(lblNombreBaraja)
-										.addGap(26)
-										.addComponent(Nombre_Baraja, GroupLayout.PREFERRED_SIZE, 303, GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.UNRELATED)
-										.addComponent(BuscarBaraja))
-								.addComponent(BarajaAleatoria, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-						.addContainerGap(47, Short.MAX_VALUE))
+										.addGap(6)
+										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addComponent(BarajaAleatoria, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
+												.addComponent(Cartas_Mazo, GroupLayout.PREFERRED_SIZE, 189, GroupLayout.PREFERRED_SIZE))))
+						.addGap(90))
 				);
 		groupLayout.setVerticalGroup(
 				groupLayout.createParallelGroup(Alignment.LEADING)
@@ -81,32 +93,53 @@ public class Pantalla_BarajaCartas extends JFrame {
 										.addComponent(Insertar)
 										.addGap(61)
 										.addComponent(Quitar)))
-						.addGap(39)
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(BarajaAleatoria)
+								.addComponent(btnLoadButton))
+						.addPreferredGap(ComponentPlacement.RELATED)
 						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 								.addComponent(lblNombreBaraja)
 								.addComponent(Nombre_Baraja, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 								.addComponent(BuscarBaraja))
-						.addGap(18)
-						.addComponent(BarajaAleatoria)
-						.addContainerGap(29, Short.MAX_VALUE))
+						.addContainerGap(55, Short.MAX_VALUE))
 				);
 		getContentPane().setLayout(groupLayout);
-		Insertar.addActionListener(new ActionListener() {
+		btnLoadButton.addActionListener(new ActionListener() {
+
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent arg0) {
 				cargarCartas();
 			}
 		});
+		Insertar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				insertarCartas();
+				
+			}
+		});
+
 		this.setVisible(true);
 		this.setSize(630, 400);
 	}
+	public void insertarCartas() {	
+		DefaultListModel modelo3 =new DefaultListModel<>();
+		modelo3.addElement(SeleccionCartas.getSelectedValue());
+		Cartas_Mazo.setModel(modelo3);
+	}
 	public  void cargarCartas() {
-		DefaultListModel modelo = new DefaultListModel();
-		modelo.addElement("Hola");
+		ArrayList pruebas = new ArrayList<>();
+		pruebas.add("Hola");
+		pruebas.add("Adios");
+		pruebas.add("Hola_Adios");
+		DefaultListModel modelo = new DefaultListModel<>();
+		for (int i = 0; i < pruebas.size(); i++) {
+			modelo.addElement(pruebas.get(i));
+		}
 		SeleccionCartas.setModel(modelo);
 	}
 	public static void main(String[] args) {
 		Pantalla_BarajaCartas b = new Pantalla_BarajaCartas();
 	}
-
 }
